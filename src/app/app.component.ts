83a66feec72b1061bbd684f8fc9f388b868ce2ee
Youssef Metwally <youssef.mostafa4140@gmail.com>
2023-08-03 08:30:11 +0300

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,42 @@ export interface FriendSchema {
 export class AppComponent {
   title = 'my-contacts-app';
   
-  friends: FriendSchema[] = [];
+  friends: FriendSchema[] = [{
+    "firstName": "ahmed",
+    "lastName": "khaled",
+    "email": "ahmed@gmail.com",
+    "number": 7777777777,
+    "editing": false,
+  },
+  {
+    "firstName": "mohammed",
+    "lastName": "ali",
+    "email": "mohammed@gmail.com",
+    "number": 8888888888,
+    "editing": false,
+  },
+  {
+    "firstName": "sara",
+    "lastName": "abdelrahman",
+    "email": "sara@gmail.com",
+    "number": 9999999999,
+    "editing": false,
+  },
+  {
+    "firstName": "emad",
+    "lastName": "hassan",
+    "email": "emad@gmail.com",
+    "number": 1111111111,
+    "editing": false,
+  },
+  {
+    "firstName": "salma",
+    "lastName": "karim",
+    "email": "salma@gmail.com",
+    "number": 2222222222,
+    "editing": false,
+  },
+  ];
 
   pushToFriends(data: FriendSchema){
     this.friends.push(data);
